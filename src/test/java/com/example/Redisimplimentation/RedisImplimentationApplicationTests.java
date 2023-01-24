0cec9d2732e3a20a43af4fc24ae76c8709864e36
Harshal Tambat <harshaltambat38@gmail.com>
2023-01-24 21:07:34 +0530

    1

---
+++ src/test/java/com/example/Redisimplimentation/RedisImplimentationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Redisimplimentation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisImplimentationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
